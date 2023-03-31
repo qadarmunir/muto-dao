917ce3ece0d48832bba3c7e1c42906d0631dd629
qadar <gq2853248@gmail.com>
2023-03-31 16:23:05 +0500

    xyz

--- meta__mask/meta1_test.py
+++ meta__mask/meta1_test.py
@@ -6,3 +6,16 @@ opt = webdriver.ChromeOptions()
 opt.add_extension(EXTENSION_PATH)
 
 driver = webdriver.Chrome(chrome_options=opt)
+driver.find_element_by_xpath('//button[text()="Get Started"]').click()
+driver.find_element_by_xpath('//button[text()="Import wallet"]').click()
+driver.find_element_by_xpath('//button[text()="No Thanks"]').click()
+
+# After this you will need to enter you wallet details
+
+inputs = driver.find_elements_by_xpath('//input')
+inputs[0].send_keys("gospel", "gold", "stone", "party", "evolve", "hungry", "mandate", "exile", "coach", "cupboard", "exit", "budget")
+inputs[1].send_keys("qadar4646")
+inputs[2].send_keys("qadar4646")
+driver.find_element_by_css_selector('.first-time-flow__terms').click()
+driver.find_element_by_xpath('//button[text()="Import"]').click()
+driver.find_element_by_xpath('//button[text()="All Done"]').click()
